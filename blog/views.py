dd6a062822fd4375dd0567b35b2781ad603bf0db
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2024-01-26 14:20:49 +0100

    Added deleteion logic

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 class BlogListView(ListView):
@@ -18,3 +19,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
