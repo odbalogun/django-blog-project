7c5a923e96be410476dda28d9632fe46b0aa64bb
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2024-01-26 14:05:59 +0100

    Added an update form

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 class BlogListView(ListView):
@@ -13,3 +13,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
