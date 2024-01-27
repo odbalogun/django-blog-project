498c5c1c3e0a057da6576dc90e74bf7034364af0
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2024-01-27 02:13:19 +0100

    Pre-deployment updates

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xkbjv3ry1@w&kavs^r)f8_2q-e@d^xm*_kvf&xny6bhe@-r&is'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".fly.dev", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]
 
 
 # Application definition
